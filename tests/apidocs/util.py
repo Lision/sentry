9627668a3af22d72e81d1429a77835220c186db0
Marcos Gaeta <marcos.gaeta@sentry.io>
2020-09-21 13:45:58 -0700

    fix create_schema

--- tests/apidocs/util.py
+++ tests/apidocs/util.py
@@ -1,25 +1,24 @@
 from __future__ import absolute_import
 
 import os
+
+from django.conf import settings
+from django.test.client import RequestFactory
 from openapi_core import create_spec
+
 from sentry.utils import json
 from sentry.testutils import APITestCase
 
 
 class APIDocsTestCase(APITestCase):
     def create_schema(self):
-        spec = None
-        # get the generated json file
-        path = os.path.join(os.path.dirname(__file__)) + "/openapi-derefed.json"
-        with open(path) as json_file:
+        path = os.path.join(os.path.dirname(__file__), "openapi-derefed.json")
+        with open(path, "r") as json_file:
             data = json.load(json_file)
-            # remove DSN and update server to testserver
-            spec = create_spec(data)
-            print(spec)
+            data["servers"][0]["url"] = settings.SENTRY_OPTIONS["system.url-prefix"]
+            del data["components"]
 
-        json_file.close()
-        return spec
-
+            return create_spec(data)
     def validate_schema(self, response):
         # TODO(meredith): Use validators to validate
         pass
